aab7ae9aea755e56174c4a1e130b0630eac76d2e
Tyler <31580343+tbssnch@users.noreply.github.com>
2025-12-11 12:24:53 -0800

    Add Typescript type definitions for API interfaces

---
+++ src/types/JSONAPI.d.ts
@@ -0,0 +1,23 @@
+interface FormMetadata {
+  title: string;
+  location: string;
+  description: string;
+  tags: string[];
+  url: string;
+  external_link: string;
+}
+
+interface UploadProgress {
+  loaded: number;
+  total: number;
+  percentage: number;
+}
+
+interface PreSignedUrlResponse {
+  uploadUrl: string;
+  fileUrl: string;
+}
+
+interface PreSignedDownloadUrlResponse {
+  downloadUrl: string;
+}
